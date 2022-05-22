326a23b01515b5f5783c231ccf8db15764bb03e8
david-breen <dvdbreen3@gmail.com>
2022-05-22 00:55:29 -0400

    initial commit

---
+++ EarthshineMeasurement/HDRImageGathering.py
@@ -0,0 +1,65 @@
+from os import listdir
+from os.path import isfile, join
+import math
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+
+# This script serves two purposes. 
+# The first of which is to reture a calibration response curve 
+# for an image sensor based on a set of images.
+# The second is to generate an HDR dataset and image
+
+
+# Load all file names from the Test images folder
+img_fn = ["TestImages/" + f for f in listdir("TestImages") if isfile(join("TestImages", f))]
+
+
+# If you would like to add the images manualy you may add them here
+"""
+img_fn = ["TestImages/image1.bmp", 
+          "TestImages/image2.bmp", 
+          "TestImages/image3.bmp", 
+          "TestImages/image4.bmp", 
+          "TestImages/image5.bmp"]
+"""
+
+img_list = [cv.imread(fn) for fn in img_fn]
+
+# This is where you add the exposure times
+exposure_times = np.array([1e-6*math.pow(2,15),
+                           1e-6*math.pow(2,16),
+                           1e-6*math.pow(2,17), 
+                           1e-6*math.pow(2,18), 
+                           0.5], dtype=np.float32)
+pixel_values = np.linspace(0,255, num=256)
+
+# Merge exposures to HDR image
+merge_debevec = cv.createMergeDebevec()
+hdr_debevec = merge_debevec.process(img_list, times=exposure_times.copy())
+
+sensor_calibration = cv.createCalibrateDebevec()
+
+# SetRandom = True allows the calibration to select random points around the image
+# SetSamples is the number of samples that the calibration will take around the image
+sensor_calibration.setRandom(True)
+sensor_calibration.setSamples(100)
+
+response_curve = sensor_calibration.process(img_list, times=exposure_times.copy())
+
+# Tonemap HDR image
+tonemap1 = cv.createTonemap(gamma=2.2)
+res_debevec = tonemap1.process(hdr_debevec.copy())
+
+# Exposure fusion using Mertens
+merge_mertens = cv.createMergeMertens()
+res_mertens = merge_mertens.process(img_list)
+
+#If you would like to generate an 8 bit image with the data uncomment these lines
+#res_debevec_8bit = np.clip(res_debevec*255, 0, 255).astype('uint8')
+#cv.imwrite("ldr_debevec.jpg", res_debevec_8bit)
+
+
+fig, rCurve = plt.subplots()  # Create a figure containing a single axes.
+rCurve.plot(response_curve[:,0,0], pixel_values)  # Plot some data on the axes.
+plt.show()
