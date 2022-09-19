76d0c79168072c3b7b9655c90974534a62995c56
david-breen <dvdbreen3@gmail.com>
2022-09-18 22:43:59 -0400

    its working a little_ total rewrite

--- Satellite/Simulation/EulerDynamicsDeriver.py
+++ Satellite/Simulation/EulerDynamicsDeriver.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 from scipy.spatial.transform import Rotation as R
 from matplotlib import pyplot as plt
 
@@ -11,11 +12,33 @@ def Euler_motion(w, M, Idiag, t):
 	return wdhold
 
 
-def RK45_step(w, M, Idiag, time, deltat):
+def RK45_step(w, M, Idiag, time, deltat, quat):
 
-    k1 = Euler_motion(w, M, Idiag, time)
-    k2 = Euler_motion(w + 0.5*k1, M, Idiag, time + 0.5*deltat)
-    k3 = Euler_motion(w + 0.5*k2, M, Idiag, time + 0.5*deltat)
-    k4 = Euler_motion(w + k3, M, Idiag, time + deltat)
+    kq1 = deltat * w_to_Qdot(w, quat)
+    kw1 = deltat * Euler_motion(w, M, Idiag, time)
 
-    return deltat * ((k1 + 2*k2 + 2*k3 + k4) / 6)
+    kq2 = deltat * w_to_Qdot(w + kw1, quat + .5*kq1)
+    kw2 = deltat * Euler_motion(w + 0.5*kw1, M, Idiag, time + 0.5*deltat)
+
+    kq3 = deltat * w_to_Qdot(w + kw2, quat + .5*kq2)
+    kw3 = deltat * Euler_motion(w + 0.5*kw2, M, Idiag, time + 0.5*deltat)
+
+    kq4 = deltat * w_to_Qdot(w + kw3, quat + kq3)
+    kw4 = deltat * Euler_motion(w + kw3, M, Idiag, time + deltat)
+
+    new_w = w + ((kw1 + 2*kw2 + 2*kw3 + kw4) / 6)
+    new_Q = quat + ((kq1 + 2*kq2 + 2*kq3 + kq4) / 6)
+    new_Q = np.divide(new_Q, np.linalg.norm(new_Q))
+    
+    return new_w, new_Q
+
+
+def w_to_Qdot(w, quat):
+
+    rot = R.from_quat(quat)
+    rot = rot.as_matrix()
+    Qdot = rot * w
+    rot = R.from_matrix(Qdot)
+    Qdot = rot.as_quat()
+
+    return Qdot
--- Satellite/Simulation/EulerDynamicsUnitTests.py
+++ Satellite/Simulation/EulerDynamicsUnitTests.py
@@ -1,4 +1,5 @@
 from cmath import cos
+from platform import win32_edition
 import numpy as np
 from scipy.spatial.transform import Rotation as R
 from matplotlib import pyplot as plt
@@ -10,69 +11,56 @@ time = np.arange(0, 100, dt)
 
 # initial conditions
 Itensor = np.array([[5, 0, 0], [0, 5, 0], [0, 0, 2]])  # kg*m^2
-initial_velocity = np.array([1, 0, 1], float)
-initial_attitude = np.array([0, 0, 0], float)
-initial_inertial = np.array([[ 0, 0, 0], [ 0, 0, 0], [ 0, 0, 0]], float)
+initial_velocity = np.array([3, 0, 2], float)
+initial_attitude = np.array([0, 0, 0, 1], float)
 M = np.array([0, 0, 0])
 
 # Matrix initalization
+
 velocity_mat = np.empty((0, 3), float)
-attitude_mat = np.empty((0, 3), float)
-inertial_mat = np.empty((0, 3, 3), float)
+attitude_mat = np.empty((0, 4), float)
 
 velocity_mat = np.append(velocity_mat, [initial_velocity], axis=0)
 attitude_mat = np.append(attitude_mat, [initial_attitude], axis=0)
-inertial_mat = np.append(inertial_mat, [initial_inertial], axis=0)
-
 
 
 
 for t in time:
+    
+    #print(RK45_step(state_mat[-1][0], M, Itensor, t, dt, state_mat[-1][1]))
 
-    if t < 100:
+    w_hold, Q_hold = RK45_step(velocity_mat[-1], M, Itensor, t, dt,
+                                attitude_mat[-1])
 
-        M = np.array([0, 0, 0])   # N*m
-    else:
+    velocity_mat = np.append(velocity_mat, [w_hold] , axis=0)
+    attitude_mat = np.append(attitude_mat, [Q_hold], axis=0)
 
-        M = np.array([-0.014, -0.2, -1])   # N*m
 
-    velocity_mat = np.append(velocity_mat, [velocity_mat[-1] + 
-						    (RK45_step(velocity_mat[-1], M, Itensor, t, dt))],
-							 axis=0)
 
-    attitude_mat = np.append(attitude_mat,
-                             [velocity_mat[-1] * dt],
-                             axis=0)
 
-    #ror = R.from_euler('xyz', (attitude_mat[-1]))
-
-   
-'''
-    inertial_mat = np.append(inertial_mat,
-                             [ror.as_matrix()],
-                             axis=0)
-'''
-w1 = np.multiply(-velocity_mat[:, 1], np.cos((1-Itensor[2][2]/Itensor[0][0])*velocity_mat[:, 2]))
-w2 = np.multiply(velocity_mat[:, 1], np.sin((1-Itensor[2][2]/Itensor[0][0])*velocity_mat[:, 2]))
-w3 = velocity_mat[:, 2]
+bigO = ((Itensor[2][2]-Itensor[0][0])/Itensor[0][0])*2
 
 time = np.append(time, time[-1] + dt)
-print(len(time))
 
-print(-velocity_mat[:, 1])
 
+'''
 fig, ax = plt.subplots(3,2)
-ax[0][0].plot(time, (velocity_mat[:, 0]))
-ax[1][0].plot(time, (velocity_mat[:, 1]))
-ax[2][0].plot(time, (velocity_mat[:, 2]))
-ax[0][1].plot(time, w1)
-ax[1][1].plot(time, w2)
-ax[2][1].plot((velocity_mat[:, 1]), (velocity_mat[:, 0]))
+ax[0][0].plot(time, velocity_mat[:, 0])
+ax[1][0].plot(time, velocity_mat[:, 1])
+ax[2][0].plot(time, velocity_mat[:, 2])
+
+ax[0][1].plot(time, 3 * np.cos(bigO * time))
+ax[1][1].plot(time, 3 * np.sin(bigO * time) )
+ax[2][1].plot(velocity_mat[:, 0], velocity_mat[:, 1])
 fig.suptitle("Numerically integrated (left) Solved (right)")
 plt.show()
 '''
+
+
+ax = plt.figure().add_subplot(projection='3d')
+
 for i in range(len(time)):
-    ax.quiver(0,0,0, attitude_mat[i, 0, 0], inertial_mat[i, 1, 0], inertial_mat[i, 2, 0])
+
+    ax.quiver(0,0,0, attitude_mat[i, 0], attitude_mat[i, 1], attitude_mat[i, 2])
     plt.draw()
     plt.pause(0.001)
-'''
