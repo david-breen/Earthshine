17a9f944bda14ffeda0281f9a8670480b58e0e43
TheChiefCode <dvdbreen3@gmail.com>
2022-09-19 13:52:20 -0400

    Cleaned up graphs

--- Satellite/Simulation/EulerDynamicsDeriver.py
+++ Satellite/Simulation/EulerDynamicsDeriver.py
@@ -1,7 +1,5 @@
 import numpy as np
-import math
 from scipy.spatial.transform import Rotation as R
-from matplotlib import pyplot as plt
 
 
 def Euler_motion(w, M, Idiag, t):
@@ -16,20 +14,16 @@ def RK45_step(w, M, Idiag, time, deltat, quat):
 
     kq1 = deltat * w_to_Qdot(w, quat)
     kw1 = deltat * Euler_motion(w, M, Idiag, time)
-
     kq2 = deltat * w_to_Qdot(w + kw1, quat + .5*kq1)
     kw2 = deltat * Euler_motion(w + 0.5*kw1, M, Idiag, time + 0.5*deltat)
-
     kq3 = deltat * w_to_Qdot(w + kw2, quat + .5*kq2)
     kw3 = deltat * Euler_motion(w + 0.5*kw2, M, Idiag, time + 0.5*deltat)
-
     kq4 = deltat * w_to_Qdot(w + kw3, quat + kq3)
     kw4 = deltat * Euler_motion(w + kw3, M, Idiag, time + deltat)
-
     new_w = w + ((kw1 + 2*kw2 + 2*kw3 + kw4) / 6)
     new_Q = quat + ((kq1 + 2*kq2 + 2*kq3 + kq4) / 6)
     new_Q = np.divide(new_Q, np.linalg.norm(new_Q))
-    
+
     return new_w, new_Q
 
 
@@ -42,3 +36,4 @@ def w_to_Qdot(w, quat):
     Qdot = rot.as_quat()
 
     return Qdot
+    
--- Satellite/Simulation/EulerDynamicsUnitTests.py
+++ Satellite/Simulation/EulerDynamicsUnitTests.py
@@ -1,9 +1,11 @@
 from cmath import cos
-from platform import win32_edition
 import numpy as np
 from scipy.spatial.transform import Rotation as R
 from matplotlib import pyplot as plt
+import matplotlib.animation as animation
+import mpl_toolkits.mplot3d.axes3d as p3
 from EulerDynamicsDeriver import *
+
 
 # Time stuff
 dt = 0.1
@@ -23,6 +25,7 @@ attitude_mat = np.empty((0, 4), float)
 velocity_mat = np.append(velocity_mat, [initial_velocity], axis=0)
 attitude_mat = np.append(attitude_mat, [initial_attitude], axis=0)
 
+display_vect = [1, 0, 0]
 
 
 for t in time:
@@ -36,15 +39,12 @@ for t in time:
     attitude_mat = np.append(attitude_mat, [Q_hold], axis=0)
 
 
-
-
 bigO = ((Itensor[2][2]-Itensor[0][0])/Itensor[0][0])*2
 
 time = np.append(time, time[-1] + dt)
 
 
-'''
-fig, ax = plt.subplots(3,2)
+fig, ax = plt.subplots(3, 2)
 ax[0][0].plot(time, velocity_mat[:, 0])
 ax[1][0].plot(time, velocity_mat[:, 1])
 ax[2][0].plot(time, velocity_mat[:, 2])
@@ -52,15 +52,7 @@ ax[2][0].plot(time, velocity_mat[:, 2])
 ax[0][1].plot(time, 3 * np.cos(bigO * time))
 ax[1][1].plot(time, 3 * np.sin(bigO * time) )
 ax[2][1].plot(velocity_mat[:, 0], velocity_mat[:, 1])
+ax[2][1].plot(3 * np.cos(bigO * time), 3 * np.sin(bigO * time))
+
 fig.suptitle("Numerically integrated (left) Solved (right)")
 plt.show()
-'''
-
-
-ax = plt.figure().add_subplot(projection='3d')
-
-for i in range(len(time)):
-
-    ax.quiver(0,0,0, attitude_mat[i, 0], attitude_mat[i, 1], attitude_mat[i, 2])
-    plt.draw()
-    plt.pause(0.001)
